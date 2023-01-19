a002066981a5087e9cad24cf13824a065a18cbd7
Caruy Chen <caruychen@gmail.com>
2023-01-19 15:41:10 +0200

    fix cors errors

--- src/services/scaleReadings.js
+++ src/services/scaleReadings.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
 const baseUrl = 'https://vfiomlqwajbenjwswajz.functions.supabase.co/index'
+const supabaseKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
 
 const parseHistData = (response) => {
     try {
@@ -13,11 +14,19 @@ const parseHistData = (response) => {
     }
 }
 
+const headers = {
+    'Authorization': `Bearer ${supabaseKey}`,
+    'Content-Type': 'application/json'
+}
+
 export const getHistory = async ({range}) => {
     const params = new URLSearchParams([
         ['range', range]
     ]);
-    const response = await axios.get(`${baseUrl}/scales/history`, { params });
+    const response = await axios.get(`${baseUrl}/scales/history`, {
+        params,
+        headers
+    });
     return parseHistData(response);
 }
 
@@ -25,12 +34,12 @@ export const getTotal = async ({range}) => {
     const params = new URLSearchParams([
         ['range', range]
     ]);
-    const response = await axios.get(`${baseUrl}/scales/accumulated`, { params });
+    const response = await axios.get(`${baseUrl}/scales/accumulated`, { params, headers });
     return parseHistData(response);
 }
 
 export const getLastestAll = async () => {
-    const response = await axios.get(`${baseUrl}/scales/latest`);
+    const response = await axios.get(`${baseUrl}/scales/latest`, { headers } );
     try {
         return (response.data);
     }
