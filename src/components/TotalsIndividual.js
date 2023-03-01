2b55f194128674e0a83618b923126566639ae3a6
Caruy Chen <caruychen@gmail.com>
2023-03-01 13:21:41 +0200

    no need for explicit rounding

--- src/components/TotalsIndividual.js
+++ src/components/TotalsIndividual.js
@@ -1,17 +1,12 @@
 import React from "react";
 import { Card, CardContent, Typography } from '@mui/material';
 
-const round = (total) => {
-    const rounded = Math.round(total * 10) / 10;
-    return rounded.toFixed(1);
-}
-
 const TotalsIndividual = ({total}) => {
     return (
         <Card m={5}>
             <CardContent>
                 <Typography variant="h5" component="div">
-                    {round(total.total)} kg
+                    {total.total.toFixed(1)} kg
                 </Typography>
                 <Typography variant="body1" component="h3">
                     {total.name}
